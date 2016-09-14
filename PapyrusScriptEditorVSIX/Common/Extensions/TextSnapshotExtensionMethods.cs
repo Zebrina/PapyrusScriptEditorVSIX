4d3ea0dad9beb27b6d19db8c8bd16c1e28d7a27f
Sabrina <Zebrina@users.noreply.github.com>
2016-09-14 13:35:34 +0200

    Update and rename SnapshotSpanExtensionMethods.cs to TextSnapshotExtensionMethods.cs

--- PapyrusScriptEditorVSIX/Common/Extensions/TextSnapshotExtensionMethods.cs
+++ PapyrusScriptEditorVSIX/Common/Extensions/TextSnapshotExtensionMethods.cs
@@ -1,12 +1,21 @@
 using Microsoft.VisualStudio.Text;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Papyrus.Common.Extensions {
-    internal static class TextSnapshotSpanExtensionMethods {
+    [DebuggerStepThrough]
+    internal static class TextSnapshotLineExtensionMethods {
+        public static SnapshotSpan ToSpan(this ITextSnapshotLine line) {
+            return new SnapshotSpan(line.Start, line.End);
+        }
+    }
+
+    [DebuggerStepThrough]
+    internal static class SnapshotSpanExtensionMethods {
         public static SnapshotSpan Subspan(this SnapshotSpan span, int offset, int length) {
             return new SnapshotSpan(span.Snapshot, new Span(span.Start.Position + offset, length));
         }
@@ -17,7 +26,7 @@ namespace Papyrus.Common.Extensions {
         public static SnapshotSpan Ignore(this SnapshotSpan span, Predicate<char> pred) {
             int offset = span.Start.Position;
             for (int i = 0; i < span.Length; ++i) {
-                if (!pred.Invoke(span.Snapshot.Item[i + offset])) {
+                if (!pred.Invoke(span.Snapshot[i + offset])) {
                     return new SnapshotSpan(span.Start.Add(i), span.End);
                 }
             }
