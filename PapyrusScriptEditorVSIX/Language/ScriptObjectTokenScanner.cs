1ae52c2c18a482938e778cefc8ce7445ebbe5ebe
Sabrina <Zebrina@users.noreply.github.com>
2016-09-14 09:45:50 +0200

    Update ScriptObjectTokenScanner.cs

--- PapyrusScriptEditorVSIX/Language/ScriptObjectTokenScanner.cs
+++ PapyrusScriptEditorVSIX/Language/ScriptObjectTokenScanner.cs
@@ -10,13 +10,15 @@ using System.Threading.Tasks;
 namespace Papyrus.Language {
     public class ScriptObjectTokenScanner : TokenScannerModule {
         public override bool Scan(SnapshotSpan sourceSnapshotSpan, int offset, ref TokenScannerState state, out Token token) {
-            string text = sourceSnapshotSpan.GetText();
-            int nextDelimiter = Delimiter.FindNext(text, offset);
-            if (nextDelimiter > offset) {
-                ScriptObject scriptObject;
-                if (ScriptObject.TryParse(text.Substring(offset, nextDelimiter - offset), out scriptObject)) {
-                    token = new Token(scriptObject, new SnapshotSpan(sourceSnapshotSpan.Snapshot, sourceSnapshotSpan.Subspan(offset, scriptObject.Text.Length)));
-                    return true;
+            if (state == TokenScannerState.Text) {
+                string text = sourceSnapshotSpan.GetText();
+                int nextDelimiter = Delimiter.FindNext(text, offset);
+                if (nextDelimiter > offset) {
+                    ScriptObject scriptObject;
+                    if (ScriptObject.TryParse(text.Substring(offset, nextDelimiter - offset), out scriptObject)) {
+                        token = new Token(scriptObject, new SnapshotSpan(sourceSnapshotSpan.Snapshot, sourceSnapshotSpan.Subspan(offset, scriptObject.Text.Length)));
+                        return true;
+                    }
                 }
             }
 
