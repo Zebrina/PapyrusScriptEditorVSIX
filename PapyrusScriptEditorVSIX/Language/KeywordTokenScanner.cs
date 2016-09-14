371d9d2d298474c8b6ff84e48d3933568c878d71
Sabrina <Zebrina@users.noreply.github.com>
2016-09-14 09:49:02 +0200

    Update KeywordTokenScanner.cs

--- PapyrusScriptEditorVSIX/Language/KeywordTokenScanner.cs
+++ PapyrusScriptEditorVSIX/Language/KeywordTokenScanner.cs
@@ -11,11 +11,13 @@ using System.Threading.Tasks;
 namespace Papyrus.Language {
     public class KeywordTokenScanner : TokenScannerModule {
         public override bool Scan(SnapshotSpan sourceSnapshotSpan, int offset, ref TokenScannerState state, out Token token) {
-            string text = sourceSnapshotSpan.GetText();
-            Keyword keyword = Keyword.Parse(text, offset, Delimiter.FindNext(text, offset) - offset);
-            if (keyword != null) {
-                token = new Token(keyword, new SnapshotSpan(sourceSnapshotSpan.Snapshot, sourceSnapshotSpan.Subspan(offset, keyword.Text.Length)));
-                return true;
+            if (state == TokenScannerState.Text) {
+                string text = sourceSnapshotSpan.GetText();
+                Keyword keyword = Keyword.Parse(text, offset, Delimiter.FindNext(text, offset) - offset);
+                if (keyword != null) {
+                    token = new Token(keyword, new SnapshotSpan(sourceSnapshotSpan.Snapshot, sourceSnapshotSpan.Subspan(offset, keyword.Text.Length)));
+                    return true;
+                }
             }
 
             token = null;
