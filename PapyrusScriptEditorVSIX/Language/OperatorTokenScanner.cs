7e8b9dd14e733e8fdca3614eaa7c25025c3198d7
Sabrina <Zebrina@users.noreply.github.com>
2016-09-14 09:46:49 +0200

    Update OperatorTokenScanner.cs

--- PapyrusScriptEditorVSIX/Language/OperatorTokenScanner.cs
+++ PapyrusScriptEditorVSIX/Language/OperatorTokenScanner.cs
@@ -10,9 +10,11 @@ using System.Threading.Tasks;
 namespace Papyrus.Language {
     public class OperatorTokenScanner : TokenScannerModule {
         public override bool Scan(SnapshotSpan sourceSnapshotSpan, int offset, ref TokenScannerState state, out Token token) {
-            Operator op = Operator.Parse(sourceSnapshotSpan.GetText(), offset);
-            if (op != null) {
-                token = new Token(op, sourceSnapshotSpan.Subspan(offset));
+            if (state == TokenScannerState.Text) {
+                Operator op = Operator.Parse(sourceSnapshotSpan.GetText(), offset);
+                if (op != null) {
+                    token = new Token(op, sourceSnapshotSpan.Subspan(offset));
+                }
             }
 
             token = null;
